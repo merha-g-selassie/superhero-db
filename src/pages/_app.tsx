235d98480e4d7047fa1601a0c76e7469333060d5
Merha <merhawi.gs@gmail.com>
2021-07-12 23:56:54 +0200

    testing react-query with ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,29 +3,37 @@ import { AppProps } from 'next/dist/next-server/lib/router/router';
 import React, { useState } from 'react';
 import GlobalTheme from '../components/GlobalTheme';
 import './_app.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Hydrate } from 'react-query/hydration';
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   const [isDarkMode] = useState(false);
 
   return (
-    <GlobalTheme isDarkMode={isDarkMode}>
-      <motion.div
-        key={router.route}
-        initial="pageInitial"
-        animate="pageAnimate"
-        variants={{
-          pageInitial: {
-            opacity: 0,
-          },
-          pageAnimate: {
-            opacity: 1,
-          },
-        }}
-      >
-        {/* <Switch checked={isDarkMode} onChange={() => setIsDarkMode(!isDarkMode)} /> */}
-        <Component {...pageProps} />
-      </motion.div>
-    </GlobalTheme>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <GlobalTheme isDarkMode={isDarkMode}>
+          <motion.div
+            key={router.route}
+            initial="pageInitial"
+            animate="pageAnimate"
+            variants={{
+              pageInitial: {
+                opacity: 0,
+              },
+              pageAnimate: {
+                opacity: 1,
+              },
+            }}
+          >
+            {/* <Switch checked={isDarkMode} onChange={() => setIsDarkMode(!isDarkMode)} /> */}
+            <Component {...pageProps} />
+          </motion.div>
+        </GlobalTheme>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,31 @@
 import { GetServerSideProps, NextPage } from 'next';
 import React from 'react';
 import { Container } from '../components/Container';
-import { SuperHeroService } from '../services/SuperHeroService';
 import { CustomError } from '../types/CustomError';
 import { SuperHero } from '../types/SuperHero';
+import { useQuery } from 'react-query';
+import { SuperHeroApi } from '../api/SuperHeroApi';
 
 interface Props {
   response: SuperHero[] | CustomError;
 }
 
 const IndexPage: NextPage<Props> = ({ response }) => {
-  return <Container response={response} />;
+  const { data, status } = useQuery('supes', SuperHeroApi.getInstance().getSuperHeroes, {
+    initialData: response,
+  });
+
+  if (status === 'error') return <div>Error...</div>;
+  if (status === 'loading') return <div>Loading...</div>;
+  return <Container response={data as SuperHero[] | CustomError} />;
 };
 
 export default IndexPage;
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const superHeroService = new SuperHeroService();
-  const response = await superHeroService.getAllSuperHeroes();
-  // { code: 404, message: 'Ooops' }
+  const supApi = SuperHeroApi.getInstance();
+  const response = await supApi.getSuperHeroes();
+
   return {
     props: {
       response: response,
